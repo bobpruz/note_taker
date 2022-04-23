bb2988eed2ee2d5fd19ab1fb1df4410942deb7e9
bobpruz <bobpruz@gmail.com>
2022-04-23 16:29:25 -0500

    built post route move app.use json empty json file

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,11 +1,26 @@
 const router = require("express").Router();
 
+const res = require("express/lib/response");
 // import database
 const data = require("../../db/db.json");
 
+// get data
 router.get("/notes", (req, res) => {
   res.json(data);
 });
 
+// add new data
+router.post("/notes", (req, res) => {
+  if(data.length === 0)
+  {
+      req.body.id = '0';
+  }
+  else
+  {
+      req.body.id = (parseInt(data[data.length-1].id)+1).toString();
+  }
+  data.push(req.body);
+  res.json(data)
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,20 +6,23 @@ const apiRoutes = require("./routes/apiRoutes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// parse json data
+app.use(express.json());
+
 // Use Routes
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.use(express.static("public"));
 
-// parse json data
-app.use(express.json());
+
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
 
+// start server
 app.listen(PORT , () => 
 {
     console.log(`API server now on port 3001!`);
